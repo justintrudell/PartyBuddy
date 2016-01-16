48cbb01d1cc3a3f527fc4fa3bce58140de065985
Justin Trudell <justin.trudell.4@gmail.com>
2016-01-16 05:06:43 -0500

    The PROJECT

---
+++ MyApplication/app/src/main/java/com/example/justin/myapplication/RideTheBusActivity.java
@@ -0,0 +1,31 @@
+package com.example.justin.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class RideTheBusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(getResources().getText(R.string.rideTheBus));
+        setContentView(R.layout.activity_ride_the_bus);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
