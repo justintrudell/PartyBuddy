397aa8b3904a25a5b56e342a82a9a91f1ae26194
Justin Trudell <justin.trudell.4@gmail.com>
2016-01-16 20:03:03 -0500

    some weird stuff is going on

--- MyApplication/app/src/main/java/com/example/justin/myapplication/RideTheBusActivity.java
+++ MyApplication/app/src/main/java/com/example/justin/myapplication/RideTheBusActivity.java
@@ -1,19 +1,13 @@
 package com.example.justin.myapplication;
 
-import android.graphics.Bitmap;
-import android.media.Image;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ExpandableListView;
-import android.widget.ImageView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class RideTheBusActivity extends AppCompatActivity {
 
@@ -94,6 +88,5 @@ public class RideTheBusActivity extends AppCompatActivity {
 
         return list;
     }
-
 }
 
